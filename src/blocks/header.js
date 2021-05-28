595fb5fc3ecd114bcb12c1ebc6cadca70258cd5f
Vladislav <vkoval2002@gmail.com>
2021-05-28 22:31:33 +0300

    Fix on list item click

--- src/blocks/header.js
+++ src/blocks/header.js
@@ -4,7 +4,7 @@ export default function Home() {
             <div className="container">
                 <header className="d-flex justify-content-center py-3">
                     <ul className="nav nav-pills">
-                        <li className="nav-item"><a href="#" className="nav-link active" aria-current="page">Home</a></li>
+                        <li className="nav-item"><a href="/" className="nav-link" aria-current="page">Home</a></li>
                         <li className="nav-item"><a href="/search" className="nav-link">Architects</a></li>
                         <li className="nav-item"><a href="#" className="nav-link">About</a></li>
                     </ul>
--- src/blocks/list.js
+++ src/blocks/list.js
@@ -1,31 +1,46 @@
 import React, {useState} from 'react';
-import ListItem from "./listItem";
+
 import photographers from "../data/architects";
 import LanguageUtil from "../util/language";
-import SearchIcon from "@material-ui/icons/Search";
+import ListItem from "./listItem";
+import Header from "./header";
+
 import {FormControl, FormGroup, Input, InputAdornment, InputLabel, makeStyles} from "@material-ui/core";
+import Typography from "@material-ui/core/Typography";
+import Grid from "@material-ui/core/Grid";
+import SearchIcon from "@material-ui/icons/Search";
+
+
 
 export default function SearchPage(props) {
     const langUtil = new LanguageUtil()
     const [value, setValue] = useState('')
+
     const filteredPhotographers = photographers
         .filter(person => person.name[langUtil.getCurrentLanguage()].toLowerCase().includes(value.toLowerCase()))
 
     return (<div>
-            <Input
-                onChange={(event) => setValue(event.target.value)}
-                id="input-with-icon-adornment"
-                startAdornment={
-                    <InputAdornment position="start">
-                        <SearchIcon/>
-                    </InputAdornment>
-                }
-            />
-            <div className="col-lg-5" style={{display: 'flex', background: 'aliceblue', borderRadius: '10px'}}>
+            <Header/>
+            <FormGroup>
+                <Typography component="h1" variant="h5">{("search-by-name")}</Typography>
+                <FormControl>
+                    <InputLabel htmlFor="input-with-icon-adornment">{"author"}</InputLabel>
+                    <Input
+                        onChange={(event) => setValue(event.target.value)}
+                        id="input-with-icon-adornment"
+                        startAdornment={
+                            <InputAdornment position="start">
+                                <SearchIcon/>
+                            </InputAdornment>
+                        }
+                    />
+                </FormControl>
+            </FormGroup>
+            <Grid container spacing={4}>
                 {filteredPhotographers.map(person => (
                     <ListItem key={person.id} author={person}/>
                 ))}
-            </div>
+            </Grid>
         </div>
     );
 }
