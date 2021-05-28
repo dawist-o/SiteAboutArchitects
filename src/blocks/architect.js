6cad4470f2f039e4c9227dfc5e9bb194dede5565
Vladislav <vkoval2002@gmail.com>
2021-05-28 22:59:28 +0300

    Add timeline

--- src/blocks/architect.js
+++ src/blocks/architect.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import LanguageUtil from "../util/language";
+import Header from "./header";
+import '../styles/timeline.css';
+import vlad from '../images/vlad.jpg';
+
 import architects from "../data/architects";
-/*import {VerticalTimeline, VerticalTimelineElement} from 'react-vertical-timeline-component';
-import 'react-vertical-timeline-component/style.min.css';*/
+import {VerticalTimeline, VerticalTimelineElement} from 'react-vertical-timeline-component';
 
 export default function Architect(props) {
     const langUtil = new LanguageUtil()
@@ -11,11 +14,21 @@ export default function Architect(props) {
     if (!author) {
         props.history.push('/')
     }
-    console.log("author = " + author)
-    console.log("langiage = " + langUtil.getCurrentLanguage())
 
-    return (<div>
-            <p>{author.name[langUtil.getCurrentLanguage()]}</p>
+    return (
+        <div>
+            <Header/>
+            <div className="text-center">
+                <img src={vlad} cclass="rounded mx-auto d-block"/>
+            </div>
+            <ul className="timeline">
+                {author.timeline.map(item =>
+                    <li className="event" data-date="12:30 - 1:00pm">
+                        <h3>{author.name[langUtil.getCurrentLanguage()]}</h3>
+                        <p>{item.description[langUtil.getCurrentLanguage()]}</p>
+                    </li>
+                )}
+            </ul>
         </div>
     )
 }
