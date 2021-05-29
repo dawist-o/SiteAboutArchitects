bb17b6431a36ae69a56807f7e6b586af213f0986
Maksim Malenho <maksim.malenho@effective-soft.com>
2021-05-29 11:37:16 +0300

    Add architect main info

--- src/blocks/architect.js
+++ src/blocks/architect.js
@@ -18,9 +18,23 @@ export default function Architect(props) {
     return (
         <div>
             <Header/>
-            <div className="text-center">
-                <img src={vlad} cclass="rounded mx-auto d-block"/>
+            <div style={{justifyContent: 'center'}}>
+                <img src={author.images[0].img} alt={author.images[0].title} style={{
+                    width: '200px',
+                    height: '200px',
+                    backgroundPosition: 'center center',
+                    backgroundRepeat: 'no-repeat',
+                    borderRadius: '50%'
+                }}/>
+                <div style={{display: 'block'}}>
+                    <h3>{author.name}</h3>
+                    <h5>{author.years}</h5>
+                </div>
             </div>
+
+            {/*<div className="text-center">
+                <img src={vlad} cclass="rounded mx-auto d-block"/>
+            </div>*/}
             <ul className="timeline">
                 {author.timeline.map(item =>
                     <li className="event" data-date="12:30 - 1:00pm">
@@ -29,6 +43,16 @@ export default function Architect(props) {
                     </li>
                 )}
             </ul>
+            <iframe width="1264" height="480" src="https://www.youtube.com/embed/dQw4w9WgXcQ"
+                    title="YouTube video player" frameBorder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                    allowFullScreen/>
+            <div className="map-responsive">
+                <iframe
+                    src="https://www.google.com/maps/embed/v1/place?key=AIzaSyA0s1a7phLN0iaD6-UE7m4qP-z21pH0eSc&q=Belarus+Minsk"
+                    width="600" height="450" frameBorder="0" style={{border: 0}}
+                    allowFullScreen/>
+            </div>
         </div>
     )
 }
